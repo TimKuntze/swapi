edc1f0073269e0225a091b923bd9b290a1e6e11f
TimKuntze <timkuntze@gmx.de>
2022-04-13 17:52:12 +0200

    added all necessary components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PeopleComponent } from './people/people.component';
+import { CharactersComponent } from './characters/characters.component';
 import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PeopleComponent,
+    CharactersComponent,
     DetailsComponent
   ],
   imports: [
